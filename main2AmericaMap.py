c7146776acb028e9b2805c5679a67dd2d81d88b1
JIVESH HAMAL <bravojivesh@gmail.com>
2024-02-15 23:53:12 +0530

    my approach adn Ang approach DONE :D

--- main2AmericaMap.py
+++ main2AmericaMap.py
@@ -2,7 +2,6 @@ import turtle as tu
 import main2Score as ans
 
 
-eog= False
 screen1=tu.Screen()
 image= "US-map.gif"
 screen1.addshape (image)
@@ -10,11 +9,15 @@ tu.shape(image)
 
 answer=ans.Score()
 
-while eog==False:
+while len(answer.correct_guess_list)<50:
 
-   ask = screen1.textinput(title=f"/50 Guess the states Foo", prompt="Enter the state name").lower()
+   ask = screen1.textinput(title=f"{len(answer.correct_guess_list)}/50 Guess the states Foo", prompt="Enter the state name").title()
 
-   answer.check_ans(ask)
-   # eog=answer.var
+   if ask== "Exit":
+      answer.missed_list_f()
+      break
+
+   else:
+      answer.check_ans(ask)
 
 tu.mainloop()
--- main2Score.py
+++ main2Score.py
@@ -2,30 +2,50 @@ import turtle as tu
 import pandas
 
 data=pandas.read_csv("50_states.csv")
-# print (data["state"])
+states= data["state"].tolist() #covert to list. In my other version, it is dictionary.
+
 class Score(tu.Turtle):
     def __init__(self):
         super().__init__()
         self.penup()
         self.hideturtle()
-        self.list=[]
+        self.correct_guess_list=[]
+        self.missed_list=[]
 
-    def check_ans(self,answer):
-        states= data["state"].tolist()
-        print(states)
-        if answer.lower() in states:
-                get_row=data[data.state==x] #gets the entire row with index and data type info
-                x_cor=int(get_row.x.item())
-                y_cor=int(get_row.y.item())
-                self.goto(x_cor,y_cor)
+    def check_ans(self,answer0):
+        answer= answer0.title()
+        # if answer.lower() in (state.lower() for state in states):
+        # this will not work because everything in
+        # the state list is title case.
+        #if we change to lower and compare, it will work for comparison ONLY.To get the cordinates, we have to use
+        # the same case as in the csv. So we have to change the input to title case.
+
+        if answer in states:
+                get_row=data[data.state==answer] #gets the entire row with index and data type info
+                x_cor=int(get_row.x.item()) #getting only the value from x and changing it to int.
+                y_cor=int(get_row.y.item()) #same as above for y.
+                self.goto(x_cor,y_cor) #after values are in int, telling the turtle to go to write cordinate.
                 self.write(answer.title())
+                # print (answer, x_cor, y_cor)
+                self.correct_guess_list.append(answer)
+                # print(self.correct_guess_list) #debuggin only:
+
+    def missed_list_f(self): #made a mistake of using the same name for function and attribute
+        for x in states:
+            if x not in self.correct_guess_list:
+                self.missed_list.append(x)
+        df=pandas.DataFrame(self.missed_list)
+        df.to_csv("Missed_state.csv")
 
 
 
 
 
-s=Score()
-s.check_ans("North Carolina")
+
+
+
+# s=Score()
+# s.check_ans("Ohio")
 
 
 
